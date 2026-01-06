633c7d6224735d66dc2eb38b0d3e81e141896a0b
nour-atef04 <nourmohamedatef04@gmail.com>
2026-01-06 23:21:30 +0200

    created homepage

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navigation from "@/app/_components/Navigation";
 import { Josefin_Sans } from "next/font/google";
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -24,14 +25,12 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} relative antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
