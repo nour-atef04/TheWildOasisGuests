0743cad58eeda88b7c31bb6d300d9af31ff2e266
nour-atef04 <nourmohamedatef04@gmail.com>
2026-01-06 23:36:32 +0200

    created the about page and cabins page

--- app/layout.js
+++ app/layout.js
@@ -25,11 +25,11 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body
-        className={`${josefin.className} relative antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefin.className}  relative antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
         <Header />
-        <div className="flex-1 px-8 py12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py12 ">
+          <main className="max-w-7xl mx-auto ">{children}</main>
         </div>
       </body>
     </html>
