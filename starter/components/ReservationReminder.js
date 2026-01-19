e3dee781ba5d139ee466ff5344cd860085c7f50f
nour-atef04 <nourmohamedatef04@gmail.com>
2026-01-20 00:55:51 +0200

    created context for date range reservation

--- starter/components/ReservationReminder.js
+++ starter/components/ReservationReminder.js
@@ -1,16 +1,17 @@
+import { useReservation } from '@/app/_components/ReservationContext';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 import { format } from 'date-fns';
 
 function ReservationReminder() {
-  // CHANGE
-  const range = { from: null, to: null };
+
+  const {range} = useReservation();
 
   if (!range.from || !range.to) return null;
 
   return (
     <div className='fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center'>
       <p>
-        <span>👋</span> Don'f forget to reserve your dates <br /> from{' '}
+        <span>👋</span> Don&apos;t forget to reserve your dates <br /> from{' '}
         {format(new Date(range.from), 'MMM dd yyyy')} to{' '}
         {format(new Date(range.to), 'MMM dd yyyy')}
       </p>
